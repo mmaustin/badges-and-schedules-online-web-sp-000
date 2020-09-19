d470c5196819501feb6a42a9a3cf585b30522399
mccray austin <tubach75@hotmail.com>
2020-09-19 17:04:14 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,3 +4,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 badge_maker("Arel")
+
+def batch_badge_creator(attendees)
+  attendees.each do |attendee|
+    "Hello, my name is #{attendee}."
+  end
+end
